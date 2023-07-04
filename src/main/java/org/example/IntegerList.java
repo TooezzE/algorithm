b271ce18e848f42c8cbf690823bb21062cf66ddc
Egor <azazaz.com534@gmail.com>
2023-07-04 21:18:55 +0300

    hw done.

--- src/main/java/org/example/IntegerList.java
+++ src/main/java/org/example/IntegerList.java
@@ -1,43 +1,52 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class IntegerList {
-    private Integer[] list = new Integer[0];
-    private int size = 0;
+    private Integer[] list = new Integer[5];
+    private int elemCount = 0;
 
     public Integer add(Integer item) {
-        Integer[] newList = Arrays.copyOf(list, size+1);
         if(item == null) {
             throw new IllegalArgumentException();
         }
         if(isEmpty()){
-            newList[0] = item;
-        } else {
-            newList[newList.length - 1] = item;
+            grow();
+            list[0] = item;
+        } else if(list[elemCount - 1] != null) {
+            list[elemCount] = item;
+        } else if(list[elemCount - 1] != null && list[list.length - 1] != null) {
+            grow();
+            list[elemCount] = item;
         }
-        list = newList;
-        size++;
+        elemCount++;
         return item;
     }
 
     public Integer add(int index, Integer item) {
-        if(index > size - 1 || item == null){
+        if(index > elemCount - 1 || item == null){
             throw new IllegalArgumentException();
         }
 
-        Integer[] newList = Arrays.copyOf(list, size+1);
-        for (int i = list.length; i > index; i--) {
-            newList[i] = newList[i-1];
+        if(list.length == elemCount) {
+            grow();
         }
-        newList[index] = item;
-        list = newList;
-        size++;
+        for (int i = list.length - 1; i > index; i--) {
+            list[i] = list[i-1];
+        }
+        list[index] = item;
+        elemCount++;
         return item;
     }
 
+    private void grow() {
+        int newLength = list.length + list.length/2;
+        list = Arrays.copyOf(list, newLength);
+    }
+
     public Integer set(int index, Integer item){
-        if(index > size - 1 || item == null){
+        if(index > elemCount - 1 || item == null){
             throw new IllegalArgumentException();
         }
         list[index] = item;
@@ -47,7 +56,7 @@ public class IntegerList {
     public Integer remove(Integer item){
         boolean isFinded = false;
         int index = 0;
-        for (int i = 0; i < list.length; i++) {
+        for (int i = 0; i < list.length - 1; i++) {
             if(list[i].equals(item)){
                 isFinded = true;
                 index = i;
@@ -57,46 +66,90 @@ public class IntegerList {
             list[j] = list[j+1];
         }
         list = Arrays.copyOf(list, list.length - 1);
-        size--;
+        elemCount--;
         if(!isFinded){
             throw new IllegalArgumentException();
         }
         return item;
     }
     public Integer remove(int index){
-        if (index > size - 1){
+        if (index > elemCount - 1){
             throw new IllegalArgumentException();
         }
         Integer temp = list[index];
         for (int i = index; i < list.length - 1; i++) {
             list[i] = list[i+1];
         }
-        list = Arrays.copyOf(list, size - 1);
-        size--;
+        list = Arrays.copyOf(list, elemCount - 1);
+        elemCount--;
         return temp;
     }
-    public IntegerList sort(IntegerList integerList) {
-        int currSize = integerList.size() - 1;
-        int max = integerList.get(0);
+    public IntegerList sort() {
+        int currSize = this.size() - 1;
+        int max = this.get(0);
         int indx = 0;
-        for (int i = 0; i < integerList.size(); i++) {
+        for (int i = 0; i < this.size(); i++) {
             for (int j = 0; j <= currSize; j++) {
-                if (integerList.get(j) > max) {
-                    max = integerList.get(j);
+                if (this.get(j) > max) {
+                    max = this.get(j);
                     indx = j;
                 }
             }
-            int tmp = integerList.get(currSize);
-            integerList.set(currSize, max);
-            integerList.set(indx, tmp);
+            int tmp = this.get(currSize);
+            this.set(currSize, max);
+            this.set(indx, tmp);
             currSize--;
             max = 0;
         }
-        return integerList;
+        return this;
+    }
+
+    public IntegerList quickSort(int[] arr, int low, int high) {
+        if(arr.length == 0) {
+            return this;
+        }
+        if(low >= high) {
+            return this;
+        }
+
+        int middleIndex = low + (high - low) / 2;
+        int pivot = arr[middleIndex];
+
+        int i = low, j = high;
+        while (i <= j) {
+            while (arr[i] < pivot) {
+                i++;
+            }
+            while(arr[j] > pivot) {
+                j--;
+            }
+
+            if(i <= j) {
+                swap(arr, i, j);
+                i++;
+                j--;
+            }
+        }
+
+        if(low < j) {
+            quickSort(arr, low, j);
+        }
+        if(high > i) {
+            quickSort(arr, i, high);
+        }
+
+        list = Arrays.copyOf(IntStream.of(arr).boxed().toArray(Integer []::new), elemCount);
+        return this;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 
     private Integer binarySearch(Integer item) {
-        sort(this);
+        sort();
         int index = -1;
         int high = list.length - 1, low = 0;
 
@@ -139,7 +192,7 @@ public class IntegerList {
     }
 
     public Integer get(int index){
-        if(index > size - 1){
+        if(index > elemCount - 1){
             throw new IllegalArgumentException();
         }
         return list[index];
@@ -149,10 +202,10 @@ public class IntegerList {
         if(otherIntegerList == null){
             throw new IllegalArgumentException();
         }
-        if(this.size != otherIntegerList.size){
+        if(this.elemCount != otherIntegerList.elemCount){
             return false;
         }
-        for (int i = 0; i < otherIntegerList.size; i++){
+        for (int i = 0; i < otherIntegerList.elemCount; i++){
             if(!this.get(i).equals(otherIntegerList.get(i))){
                 return false;
             }
@@ -161,11 +214,15 @@ public class IntegerList {
     }
 
     public int size(){
-        return size;
+        return elemCount;
+    }
+
+    public int getListLength() {
+        return list.length;
     }
 
     public boolean isEmpty(){
-        if(size == 0){
+        if(elemCount == 0){
             return true;
         }
         int temp = 0;
@@ -174,16 +231,22 @@ public class IntegerList {
                 temp++;
             }
         }
-        return temp == size;
+        return temp == elemCount;
     }
 
     public void clear(){
         list = new Integer[0];
-        size = 0;
+        elemCount = 0;
     }
 
-    public Integer[] toArray(){
-        return Arrays.copyOf(list, size);
+    public Integer[] toObjectArray() {
+        return Arrays.copyOf(list, elemCount);
     }
+
+    public int[] toPrimitiveArray() {
+        Integer[] result = Arrays.copyOf(list, elemCount);
+        return Arrays.stream(result).mapToInt(i->i).toArray();
+    }
+
 }
 
